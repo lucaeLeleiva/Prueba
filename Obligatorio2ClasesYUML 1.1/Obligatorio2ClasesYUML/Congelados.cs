a3eaf862c39a9512d1049ca474784ef1af2a5a87
lucaeLeleiva <lucaleiva1@hotmail.com>
2016-07-18 10:47:03 -0300

    Update Congelados.cs

--- Obligatorio2ClasesYUML 1.1/Obligatorio2ClasesYUML/Congelados.cs
+++ Obligatorio2ClasesYUML 1.1/Obligatorio2ClasesYUML/Congelados.cs
@@ -13,18 +13,24 @@ namespace Obligatorio2ClasesYUML
 		#region Propiedades
 			public int PesoEnGramos{
 				get{return pesoEnGramos;}
-				set{pesoEnGramos=value;}
+				set{
+					if(value>0){//El peso no puede ser negativo
+						pesoEnGramos=value;
+					}else{
+						throw new Exception("El peso no puede ser 0 ni menor.");
+					}
+				}
 			}
 		#endregion
 		#region Constructores
 			public Congelados(int pesoEnGramos,string codigoDeBarras,DateTime fechaDeVencimiento,string nombre,double precio):base(codigoDeBarras, fechaDeVencimiento, nombre, precio){
-			this.PesoEnGramos=pesoEnGramos;
-		}
+				this.PesoEnGramos=pesoEnGramos;
+			}
 		#endregion
 		#region Operaciones
 			public override string  ToString(){
 				string frase;
-                frase=base.ToString()+" y pesa "+pesoEnGramos+"g";
+                frase=base.ToString()+" y pesa "+pesoEnGramos+"g.";
                 return frase;
             }
 				
